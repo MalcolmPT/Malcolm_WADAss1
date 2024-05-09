d91e9a15b61116241acfbd0233931edb0af3055c
MalcolmPT <malcolmpyeth4nt@gmail.com>
2024-05-09 14:56:28 +0800

    Update Malcolm_WAD.js

--- Malcolm_WAD.js
+++ Malcolm_WAD.js
@@ -4,24 +4,18 @@ module.exports = {
         const AfterServiceCharge = amount * ((ServiceCharge + 100 )/ 100);
         const AfterGST = AfterServiceCharge * ((GSTrate + 100)/ 100);
         const Total = tips + AfterGST;
-        console.log(Total);
+        let formattednum = Total.toFixed(2);
+        console.log("Total Cost: $" + formattednum);
+        
 },
     //this is food menu
     FoodItems: [
         { "Name": "Truffle Fries", "Price": 4 },
         { "Name": "Fettuccine Alfredo", "Price": 12 },
-        { "Name": "Wagyu Steak with Potato Wedges", "Price": 22 },
+        { "Name": "Wagyu Steak ", "Price": 22 },
         { "Name": "Cheeseburger", "Price": 16 },
         { "Name": "Caesar salad", "Price": 9 },
-        { "Name": "Grilled Salmon with Mashed Potatoes", "Price": 20 }
-    ],
-    // Takes account users minimum and maximum budget and finds food items that meet the budget range key: int value
-    minmaxbudget(MinimumBudget, Maximumbudget) {
-        const result = this.FoodItems.filter(item => item.Price >= MinimumBudget && item.Price <= Maximumbudget);
-        console.log(result);   
-    },
-
-    DrinkItems: [
+        { "Name": "Grilled Salmon ", "Price": 20 },
         { "Name": "Iced Lemon Tea", "Price": 3, "alocoholic": "No" },
         { "Name": "Coca Cola", "Price": 2, "alocoholic": "No" },
         { "Name": "Vanilla Milkshake", "Price": 6, "alocoholic": "No" },
@@ -31,13 +25,30 @@ module.exports = {
         { "Name": "Bloody Mary", "Price": 20, "alocoholic": "Yes" },
         { "Name": "Piña Colada", "Price": 21, "alocoholic": "Yes" },
         { "Name": "Martini", "Price": 24, "alocoholic": "Yes" },
+        { "Name": "Margarita", "Price": 24, "alocoholic": "Yes" },
         { "Name": "Wine", "Price": 23, "alocoholic": "Yes" }
+        
     ],
+    // Takes account users minimum and maximum budget and finds food items that meet the budget range key: int value
+    minmaxbudget(MinimumBudget, Maximumbudget) {
+        
+        const result = this.FoodItems.filter(item => item.Price >= MinimumBudget && item.Price <= Maximumbudget);
+        for (const item of result){
+            const x = [];
+            const y = [];
+            x.push(item.Name);
+            y.push(item.Price);
+            console.log("Item: " + x[x.length-1] + "/ $" + y[y.length-1]);  
+        }
+        
+    },
+
+
     //Seperates alcohlic and non-alcoholic beverages by using key: string value 
     NonAlcoholicList(){
         const nonAlcoholicDrinks = [];
         const AlcoholicDrinks = [];
-        for (const item of this.DrinkItems){
+        for (const item of this.FoodItems){
             if (item.alocoholic == "No"){
                 nonAlcoholicDrinks.push(" " + item.Name );
             }
@@ -48,7 +59,7 @@ module.exports = {
         return console.log("These are the non-Alcoholic drinks\n" + nonAlcoholicDrinks + "\n\nThese are the Alcoholic drinks\n" + AlcoholicDrinks);
         
     },
-    //Allows users to send Name, Email and feedback and store it within an array as json objects
+    //Allows users to send Name, Email and feedback and store it within an array as json objects Feedbacks can be accessed with a password(Clearance)
     GiveFeedback(Name, Email, Feedback, password = ""){
         ListOfFeedbacks = []
         if (password != "P@ssw0rd!" || password == "")
@@ -58,15 +69,23 @@ module.exports = {
                 ListOfFeedbacks.push(Json);
             }
         console.log(ListOfFeedbacks);
+    },
+    //Chooses a random cocktail for the customer
+    CocktailParty(){
+        const cocktails = ["Mojito", "Bloody Mary", "Piña Colada", "Martini", "Margarita"];
+        const randomIndex = Math.floor(Math.random() * cocktails.length);
+        console.log(cocktails[randomIndex]);
     }
 
+    
 }
 
 module.exports.calculateTotalBill(100,9,10,40);
-module.exports.minmaxbudget(10,15);
+module.exports.minmaxbudget(20,25);
 module.exports.NonAlcoholicList();
 module.exports.GiveFeedback("Malcolm", "221737Y@mymail.nyp.edu.sg", "The food tasted like it was made with love");
 module.exports.GiveFeedback("Darren", "D4rren@gmail.com", "I didn't like the food");
-module.exports.GiveFeedback("Russell", "Ruzzel@gmail.com", "Amazing variety of drinks!");
+module.exports.GiveFeedback(123, "Ruzzel@gmail.com", "Amazing variety of drinks!");
 module.exports.GiveFeedback("Darren", "D4rren@gmail.com", "I didn't like the food", "P@ssw0rd!");
+module.exports.CocktailParty();
 // console.log(module.exports.ListOfFeedbacks);
